3e9ba482ad5767ab2625f10e05a4dd66fbd7d737
hongshengliu <hongshengliu@liontravel.com>
2022-01-03 14:38:43 +0800

    add RWD feature

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -24,7 +24,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="todo-header">
-                <input onKeyUp={this.handleKeyUp} type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+                <input onKeyUp={this.handleKeyUp} type="text" placeholder="請輸入您的代辦名稱，按下Enter送出"/>
             </div>
         )
     }
